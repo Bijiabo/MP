b9b3f21d09580c1fbd86959a73acb7d3ae1359e5
bijiabo <bijiabo@gmail.com>
2015-06-12 16:23:38 +0800

    记录孩子信息

--- MP/userInformationViewController.swift
+++ MP/userInformationViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class userInformationViewController: UIViewController {
 
+    @IBOutlet var childNameTextField: UITextField!
+    @IBOutlet var childSexualitySegmentedControl: UISegmentedControl!
+    @IBOutlet var childBirthdayDatePicker: UIDatePicker!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        
+        initView()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,8 +26,51 @@ class userInformationViewController: UIViewController {
 
     }
     
+    func initView () -> Void
+    {
+        initDatePicker()
+        
+        if let childName =  NSUserDefaults.standardUserDefaults().stringForKey("childName")
+        {
+            childNameTextField.text = childName
+        }
+        
+        if let childSexuality = NSUserDefaults.standardUserDefaults().stringForKey("childSexuality")
+        {
+            for var i = 0 ; i < childSexualitySegmentedControl.numberOfSegments ; i++
+            {
+                if childSexualitySegmentedControl.titleForSegmentAtIndex(i) == childSexuality
+                {
+                    childSexualitySegmentedControl.selectedSegmentIndex = i
+                    break
+                }
+            }
+        }
+        
+    }
+    
+    func initDatePicker () -> Void
+    {
+        let ageMax : Int = 4
+        let minimumDate : NSDate = NSDate(timeIntervalSinceNow: NSTimeInterval( -3600*24*365*(ageMax+1) + 3600*24*1 ))
+        childBirthdayDatePicker.minimumDate = minimumDate
+        childBirthdayDatePicker.maximumDate = NSDate()
+        
+        if let childBirthday: AnyObject = NSUserDefaults.standardUserDefaults().objectForKey("childBirthday")
+        {
+            childBirthdayDatePicker.date = childBirthday as! NSDate
+        }
+    }
+    
 
     @IBAction func tapSaveButton(sender: UIBarButtonItem) {
+        
+        NSUserDefaults.standardUserDefaults().setObject(childNameTextField.text, forKey: "childName")
+        
+        let childSexuality : String =  childSexualitySegmentedControl.titleForSegmentAtIndex(childSexualitySegmentedControl.selectedSegmentIndex)!
+        NSUserDefaults.standardUserDefaults().setObject(childSexuality, forKey: "childSexuality")
+        
+        NSUserDefaults.standardUserDefaults().setObject(childBirthdayDatePicker.date, forKey: "childBirthday")
         
         self.dismissViewControllerAnimated(true, completion: nil)
     }
